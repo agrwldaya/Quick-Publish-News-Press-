8587f2ef4e4f9ac438b77b77ef0fc96e13714e1c
agrwldaya <agrawaldaya336@gmail.com>
2024-11-27 18:38:08 +0530

    added

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
-import cors from 'cors';
+import cors from 'cors';  // Importing the CORS package
 import dbconnect from './config/database.js';
 import fileUpload from 'express-fileupload';
 import { cloudinaryconnect } from './config/cloudinary.js';
@@ -9,8 +9,6 @@ import { ClientRoute } from './routes/clientAuth.js';
 import { empRoute } from './routes/empRoute.js';
 import { NewsPaperRoute } from './routes/NewsRoute.js';
 import paymentRoute from './routes/paymentRoute.js';
- 
- 
 
 const app = express();
 const Port = process.env.PORT || 4001;
@@ -18,26 +16,24 @@ const Port = process.env.PORT || 4001;
 // Middleware configuration
 app.use(express.json());
 
+// Use CORS middleware
+app.use(cors());  // This will allow all origins by default
+
 app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: '/tmp/'
 }));
- 
 
 // Connect to database and Cloudinary
 dbconnect();
 cloudinaryconnect();
 
 // Define routes
- 
- 
-app.use('/api/v1/client',  ClientRoute);
-app.use('/api/v1/normaluser',NormalUserRoute);
+app.use('/api/v1/client', ClientRoute);
+app.use('/api/v1/normaluser', NormalUserRoute);
 app.use('/api/v1/employee', empRoute);
-app.use('/api/v1',NewsPaperRoute)
-app.use('/api/v1',paymentRoute)
- 
- 
+app.use('/api/v1', NewsPaperRoute);
+app.use('/api/v1', paymentRoute);
 
 // Start the server
 app.listen(Port, () => {
